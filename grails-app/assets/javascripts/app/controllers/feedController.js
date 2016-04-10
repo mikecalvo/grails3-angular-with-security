1246781877536f6c2f54823be011ea727a7d0e86
Mike Calvo <mike.calvo@target.com>
2016-04-09 19:35:16 -0500

    Removed some commented code

--- grails-app/assets/javascripts/app/controllers/feedController.js
+++ grails-app/assets/javascripts/app/controllers/feedController.js
@@ -1,7 +1,7 @@
 angular.module('app').controller('feedController', function ($resource, $scope) {
 
-  // var User = $resource('/user/:userId', {userId:'@id'});
   var Restaurant = $resource('/api/restaurants/:restaurantId', {restaurantId: '@id'});
 
   $scope.restaurants = Restaurant.query();
+  
 });
